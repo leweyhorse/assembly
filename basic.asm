e6b5f344ac4588298896f6d1011ce44fe056f9a0
leweyhorse <44213730+leweyhorse@users.noreply.github.com>
2022-03-29 06:37:09 -0600

    basic.asm
    asm hello world for x86

---
+++ basic.asm
@@ -0,0 +1,21 @@
+; a basic assembly file
+
+section .data
+  msg db "Hello World!!",10,0 ;10 is nl, 0 terminates string
+
+section .bss
+
+section .text
+  GLOBAL _start       ;using _start for ld linker
+                      ;use main for gcc linking
+_start:
+
+  mov rax, 1          ;write
+  mov rdi, 1          ;to screen
+  mov rsi, msg        ;string to display
+  mov rdi, 13         ;length of string
+  syscall
+
+  mov rax, 60         ;exit
+  mov rdx, 0          ;return 0
+  syscall
